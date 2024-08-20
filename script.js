55c4ffb9fb2d66957dc9e027d2d5d8132c9e0903
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-08-20 22:06:43 +0200

    change function and change css

--- script.js
+++ script.js
@@ -2,23 +2,22 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const mainContent = document.querySelector('.mainContent');
 
-left.addEventListener('mouseenter', () => mainContent.classList.add('hover-left'));
-left.addEventListener('mouseleave', () => mainContent.classList.remove('hover-left'));
+function cursorMovedEnter(side) {
+    mainContent.classList.add(`hover-${side}`) //uso de template literals para crear dinamicamente el nombre de la clase
+}
 
-right.addEventListener('mouseenter', () => mainContent.classList.add('hover-right'));
-right.addEventListener('mouseleave', () => mainContent.classList.remove('hover-right'));
+function cursorMovedOut(side) {
+    mainContent.classList.remove(`hover-${side}`)
+}
 
-// function cursorMovedEnter(side) {
-//     mainContent.classList.add(`hover${side}`) //uso de template literals para crear dinamicamente el nombre de la clase
-// }
+left.addEventListener('mouseenter', () => cursorMovedEnter('left'));
+left.addEventListener('mouseleave', () => cursorMovedOut('left'));
 
-// function cursorMovedOut(side) {
-//     mainContent.classList.remove(`hover${side}`)
-// }
+right.addEventListener('mouseenter', () => cursorMovedEnter('right'));
+right.addEventListener('mouseleave', () => cursorMovedOut ('right'));
 
-// left.addEventListener('mouseenter', () => cursorMovedEnter('left'));
-// left.addEventListener('mouseleave', () => cursorMovedOut('left'));
+// left.addEventListener('mouseenter', () => mainContent.classList.add('hover-left'));
+// left.addEventListener('mouseleave', () => mainContent.classList.remove('hover-left'));
 
-// right.addEventListener('mouseenter', () => cursorMovedEnter('right'));
-// right.addEventListener('mouseleave', () => cursorMovedOut ('right'));
-
+// right.addEventListener('mouseenter', () => mainContent.classList.add('hover-right'));
+// right.addEventListener('mouseleave', () => mainContent.classList.remove('hover-right'));
